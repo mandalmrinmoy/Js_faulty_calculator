f740ec90daad8bf3753beab44cf45e38bd020072
mandalmrinmoy <112158728+mandalmrinmoy@users.noreply.github.com>
2024-05-14 14:18:29 +0530

    Add files via upload

---
+++ faulty_calculator.js
@@ -0,0 +1,17 @@
+let a=1;
+let b=1;
+let Random=Math.random();
+for(let i=0;i<50;i++){
+if(Random<0.1){
+    console.log("Addition: "+ (a-b) );
+    // console.log("subtraction: "+ (a+b) );
+    // console.log("Multiplicatiob: "+ (a/b) );
+    // console.log("Division: "+ (a*b) );
+}
+else{
+    console.log("Addition: "+ (a+b) );
+    // console.log("subtraction: "+ (a-b) );
+    // console.log("Multiplicatiob: "+ (a*b) );
+    // console.log("Division: "+ (a/b) );
+}
+}
